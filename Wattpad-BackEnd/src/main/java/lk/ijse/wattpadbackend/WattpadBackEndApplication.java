b8e889f54786d630c6935669e5a0d59a14a6b116
thenurinethangi <thenurinathangi@gmail.com>
2025-07-18 12:35:15 +0530

    project setup done

---
+++ Wattpad-BackEnd/src/main/java/lk/ijse/wattpadbackend/WattpadBackEndApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.wattpadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattpadBackEndApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WattpadBackEndApplication.class, args);
+    }
+
+}
---
+++ Wattpad-BackEnd/src/test/java/lk/ijse/wattpadbackend/WattpadBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.wattpadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattpadBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
