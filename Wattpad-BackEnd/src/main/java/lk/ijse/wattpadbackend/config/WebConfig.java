b8916312f05775e2eb36d6a5c6321ed52c9d757b
thenurinethangi <thenurinathangi@gmail.com>
2025-09-06 00:49:30 +0530

    add essential configs

--- Wattpad-BackEnd/src/main/java/lk/ijse/wattpadbackend/config/WebConfig.java
+++ Wattpad-BackEnd/src/main/java/lk/ijse/wattpadbackend/config/WebConfig.java
@@ -3,10 +3,11 @@ package lk.ijse.wattpadbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -19,6 +20,16 @@ public class WebConfig {
                         .allowCredentials(true);
             }
         };
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/assets/image/**")
+                .addResourceLocations(
+                        "classpath:/static/assets/image/",      // for static images in project
+                        "file:/C:/wattpad-uploads/"              // for uploaded images
+                );
     }
 }
 
